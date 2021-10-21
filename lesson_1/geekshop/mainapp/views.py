e8aab8ff0aa8e9904490ac22177c01aabcfd9ec8
still13free <revenger-x-13@mail.ru>
2021-10-21 13:49:48 +0500

    Практическое задание 2

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -2,12 +2,78 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', context=context)
+
+
+links_menu = [
+    {
+        'url': 'products',
+        'title': 'все',
+    },
+    {
+        'url': 'products_home',
+        'title': 'дом',
+    },
+    {
+        'url': 'products_office',
+        'title': 'офис',
+    },
+    {
+        'url': 'products_modern',
+        'title': 'модерн',
+    },
+    {
+        'url': 'products_classic',
+        'title': 'классика',
+    },
+]
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
 
 
-def contact(request):
-    return render(request, 'mainapp/contact.html')
+def products_home(request):
+    context = {
+        'title': 'Продукты для дома',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_office(request):
+    context = {
+        'title': 'Продукты для офиса',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_modern(request):
+    context = {
+        'title': 'Продукты "модерн"',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_classic(request):
+    context = {
+        'title': 'Продукты "классика"',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def contacts(request):
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contacts.html', context=context)
