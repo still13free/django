02bb731b0ed728784de568d3e137bddfd9d3c781
still13free <revenger-x-13@mail.ru>
2022-01-09 11:47:53 +0500

    [fix] к ролику

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -28,7 +28,7 @@ def add(request, pk):
     if not basket_item:
         basket_item = Basket(product=product_item, user=request.user)
 
-    basket_item.quantity += F('quantity') + 1
+    basket_item.quantity = F('quantity') + 1
     basket_item.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
